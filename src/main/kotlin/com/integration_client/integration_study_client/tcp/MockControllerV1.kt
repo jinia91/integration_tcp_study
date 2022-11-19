5ff579235cac5185f29b15e5980beb714cad863d
jinia91 <creee77@gmail.com>
2022-11-19 19:35:50 +0900

    version 1.0 형상 기록

--- src/main/kotlin/com/integration_client/integration_study_client/tcp/MockControllerV1.kt
+++ src/main/kotlin/com/integration_client/integration_study_client/tcp/MockControllerV1.kt
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class MockController(
+class MockControllerV1(
     private val tcpClient: TcpClient
 ) {
 
